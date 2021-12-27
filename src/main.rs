47b3aafc77b93123c5ec4c1471fe715262d12152
stouma915 <sasakitouma@yahoo.ne.jp>
2021-12-27 15:04:22 +0900

    ファイルの内容を読み取り実行可能にする

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use clap::{App, Arg};
 use colored::Colorize;
 use std::io::Write;
 use std::process::exit;
-use std::{env, io};
+use std::{fs, io};
 
 use crate::memory::Memory;
 
@@ -11,10 +12,36 @@ mod memory;
 mod util;
 
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
-    args.remove(0);
+    let matches = App::new("Brainf**k Interpreter")
+        .version(option_env!("CARGO_PKG_VERSION").unwrap_or("1.0.0"))
+        .arg(
+            Arg::with_name("SOURCE")
+                .help("Brainfuck source file (Run interactive if not specified)"),
+        )
+        .get_matches();
 
-    if args.is_empty() {
+    if let Some(source) = matches.value_of("SOURCE") {
+        match fs::read_to_string(source) {
+            Ok(content) => {
+                let mut memory = Memory::new();
+                let run_result = interpreter::run(&content, &mut memory);
+
+                match run_result {
+                    Ok(data) => {
+                        println!("{}", data.content);
+                    }
+                    Err(error) => {
+                        println!("{}", "Execution failed.".bright_red());
+                        println!("Message: {}", error.message);
+                    }
+                }
+            }
+            Err(err) => {
+                println!("Unable to read source file: {:?}", err.kind());
+                exit(1);
+            }
+        }
+    } else {
         exit(interactive_interpreter());
     }
 }
@@ -40,6 +67,7 @@ fn interactive_interpreter() -> i32 {
         } else {
             let mut memory = Memory::new();
             let run_result = interpreter::run(&trimmed, &mut memory);
+
             match run_result {
                 Ok(data) => {
                     println!();
