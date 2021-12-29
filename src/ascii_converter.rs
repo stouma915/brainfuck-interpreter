337ad5c6df9f23d9d333fcd379292aea1685f45b
stouma915 <sasakitouma@yahoo.ne.jp>
2021-12-29 14:46:29 +0900

    Update ascii_converter.rs

--- src/ascii_converter.rs
+++ src/ascii_converter.rs
@@ -2,19 +2,17 @@ use lazy_static::lazy_static;
 use unescape::unescape;
 
 lazy_static! {
-    static ref LOOKUP_TABLE: Vec<String> = (0..127).map(|n| format!("\\x{:02x}", n)).collect();
+    static ref LOOKUP_TABLE: Vec<char> = (0..127)
+        .map(|n| format!("\\x{:02x}", n))
+        .map(|n| unescape(n.as_str()).unwrap())
+        .map(|n| n.chars().collect::<Vec<char>>())
+        .map(|n| *n.get(0).unwrap())
+        .collect();
 }
 
 pub fn convert_to_char(i: i16) -> Option<char> {
     if i >= 0 && i <= 127 {
-        let escaped = LOOKUP_TABLE.get(i as usize).unwrap().to_owned();
-
-        let chars = unescape(escaped.as_str())
-            .unwrap()
-            .chars()
-            .collect::<Vec<char>>();
-
-        Some(*chars.get(0).unwrap())
+        Some(*LOOKUP_TABLE.get(i as usize).unwrap())
     } else {
         None
     }
