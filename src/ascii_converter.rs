da6cd0b177dbd52faa0d7eed471391844b43fbaa
stouma915 <sasakitouma@yahoo.ne.jp>
2021-12-29 14:54:20 +0900

    lazy_staticの代わりにonce_cellを使用

--- src/ascii_converter.rs
+++ src/ascii_converter.rs
@@ -1,14 +1,14 @@
-use lazy_static::lazy_static;
+use once_cell::sync::Lazy;
 use unescape::unescape;
 
-lazy_static! {
-    static ref LOOKUP_TABLE: Vec<char> = (0..127)
+static LOOKUP_TABLE: Lazy<Vec<char>> = Lazy::new(|| {
+    (0..127)
         .map(|n| format!("\\x{:02x}", n))
         .map(|n| unescape(n.as_str()).unwrap())
         .map(|n| n.chars().collect::<Vec<char>>())
         .map(|n| *n.get(0).unwrap())
-        .collect();
-}
+        .collect()
+});
 
 pub fn convert_to_char(i: i16) -> Option<char> {
     LOOKUP_TABLE.get(i as usize).map(|e| *e)
