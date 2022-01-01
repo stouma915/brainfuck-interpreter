9b3c399f9d464ae1fa8bb2fdf04d2cf51b95ddb9
stouma915 <sasakitouma@yahoo.ne.jp>
2022-01-01 17:54:55 +0900

    テストを追加

--- src/ascii_converter.rs
+++ src/ascii_converter.rs
@@ -1,6 +1,19 @@
 use once_cell::sync::Lazy;
 use unescape::unescape;
 
+#[cfg(test)]
+mod tests {
+    use crate::ascii_converter::convert_to_char;
+
+    #[test]
+    fn convert_to_ascii_correctly() {
+        assert_eq!(convert_to_char(65), Some(&'A'));
+        assert_eq!(convert_to_char(66), Some(&'B'));
+        assert_eq!(convert_to_char(67), Some(&'C'));
+        assert_eq!(convert_to_char(68), Some(&'D'));
+    }
+}
+
 static LOOKUP_TABLE: Lazy<Vec<char>> = Lazy::new(|| {
     (0..127)
         .map(|n| format!("\\x{:02x}", n))
